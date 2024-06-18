72b0535e0e5026b435772466b996c35b88b84ab5
pr19gya <pragya.shri2001@gmail.com>
2024-06-18 20:17:01 +0530

    Update-1

---
+++ src/main/java/com/movie_backend/movies/ReviewController.java
@@ -0,0 +1,9 @@
+package com.movie_backend.movies;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class ReviewController {
+}
